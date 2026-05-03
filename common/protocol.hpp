2d5dbc6fe1b4de498fb073255f51c82352188e00
Hugo Gabriel Pereira de Almeida <hugogpdealmeida@gmail.com>
2026-05-03 22:02:47 +0200

    Format code in document

--- common/protocol.hpp
+++ common/protocol.hpp
@@ -2,44 +2,78 @@
 #include <cstdint>
 #include <cstring>
 
-namespace uds_hft {
+namespace uds_hft
+{
 
-static constexpr int DEPTH = 15;
+    static constexpr int DEPTH = 15;
 
-struct BookSide {
-    float distance[DEPTH];        // bids/asks_distance_0..14
-    float notional[DEPTH];        // bids/asks_notional_0..14
-    float cancel_notional[DEPTH]; // bids/asks_cancel_notional_0..14
-    float limit_notional[DEPTH];  // bids/asks_limit_notional_0..14
-    float market_notional[DEPTH]; // bids/asks_market_notional_0..14
-};
+    static constexpr uint8_t MSG_SNAPSHOT = 0x01;
+    static constexpr uint8_t MSG_ORDER = 0x02;
+    static constexpr uint8_t MSG_HANDSHAKE = 0xAA;
+    static constexpr uint8_t MSG_HEARTBEAT = 0xBB;
+    static constexpr uint8_t MSG_ACK = 0xCC;
 
-struct MarketSnapshot {
-    uint8_t  msg_type;      // 0x01 = snapshot
-    uint8_t  symbol;        // 0=ADA, 1=BTC, 2=ETH
-    uint8_t  timeframe;     // 0=1s, 1=1min, 2=5min
-    uint8_t  _pad;
-    int64_t  system_time_us; // timestamp 
-    double   midpoint;
-    double   spread;
-    double   buys;
-    double   sells;
-    BookSide bids;
-    BookSide asks;
-};
+    enum class Symbol : uint8_t
+    {
+        ADA = 0,
+        BTC = 1,
+        ETH = 2,
+        ALL = 255
+    };
+    enum class Timeframe : uint8_t
+    {
+        S1 = 0,
+        MIN1 = 1,
+        MIN5 = 2,
+        ALL = 255
+    };
 
-struct OrderRequest {
-    uint8_t  msg_type;   // 0x02 = order
-    uint8_t  side;       // 0=buy, 1=sell
-    uint8_t  _pad[2];
-    double   price;
-    double   quantity;
-    int64_t  timestamp_us;
-};
+    struct BookSide
+    {
+        float distance[DEPTH];
+        float notional[DEPTH];
+        float cancel_notional[DEPTH];
+        float limit_notional[DEPTH];
+        float market_notional[DEPTH];
+    };
 
-struct MsgHeader {
-    uint8_t msg_type;
-    uint32_t payload_len; 
-} __attribute__((packed));
+    struct MarketSnapshot
+    {
+        uint8_t msg_type;
+        uint8_t symbol;
+        uint8_t timeframe;
+        uint8_t _pad;
+        int64_t system_time_us;
+        double midpoint;
+        double spread;
+        double buys;
+        double sells;
+        BookSide bids;
+        BookSide asks;
+    };
+
+    struct OrderRequest
+    {
+        uint8_t msg_type;
+        uint8_t side;
+        uint8_t _pad[2];
+        double price;
+        double quantity;
+        int64_t timestamp_us;
+    };
+
+    struct HandshakeMsg
+    {
+        uint8_t msg_type;
+        uint8_t symbol;
+        uint8_t timeframe;
+        uint8_t _pad;
+    };
+
+    struct MsgHeader
+    {
+        uint8_t msg_type;
+        uint32_t payload_len;
+    } __attribute__((packed));
 
 } // namespace uds_hft
