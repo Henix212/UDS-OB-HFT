75870501e2d8b6f27064c4e871f5f8974baa191e
Hugo Gabriel Pereira de Almeida <hugogpdealmeida@gmail.com>
2026-05-03 21:59:55 +0200

    format code in file

--- server/include/session_manager.hpp
+++ server/include/session_manager.hpp
@@ -6,47 +6,47 @@
 #include <atomic>
 #include "../../common/protocol.hpp"
 
-namespace uds_hft {
+namespace uds_hft
+{
 
-enum class Symbol    : uint8_t { ADA=0, BTC=1, ETH=2, ALL=255 };
-enum class Timeframe : uint8_t { S1=0, MIN1=1, MIN5=2, ALL=255 };
+    struct Session
+    {
+        int fd;
+        Symbol symbol;
+        Timeframe timeframe;
 
-struct Session {
-    int      fd;
-    Symbol   symbol;
-    Timeframe timeframe;
+        uint64_t snapshots_sent{0};
+        uint64_t snapshots_dropped{0}; // client trop lent
+        std::chrono::steady_clock::time_point last_heartbeat;
 
-    uint64_t snapshots_sent{0};
-    uint64_t snapshots_dropped{0};  // client trop lent
-    std::chrono::steady_clock::time_point last_heartbeat;
+        bool ready{false};
+    };
 
-    bool ready{false};
-};
+    class SessionManager
+    {
+    public:
+        explicit SessionManager(int heartbeat_interval_ms = 1000);
+        ~SessionManager();
 
-class SessionManager {
-public:
-    explicit SessionManager(int heartbeat_interval_ms = 1000);
-    ~SessionManager();
+        void on_connect(int fd);
 
-    void on_connect(int fd);
+        int dispatch(const MarketSnapshot &snap);
 
-    int  dispatch(const MarketSnapshot& snap);
+        void on_disconnect(int fd);
 
-    void on_disconnect(int fd);
+        void print_stats() const;
 
-    void print_stats() const;
+    private:
+        void heartbeat_loop();
+        bool do_handshake(Session &sess);
+        bool send_all(int fd, const void *buf, size_t len);
 
-private:
-    void heartbeat_loop();
-    bool do_handshake(Session& sess);
-    bool send_all(int fd, const void* buf, size_t len);
+        std::unordered_map<int, Session> sessions_;
+        mutable std::mutex sessions_mtx_;
 
-    std::unordered_map<int, Session> sessions_;
-    mutable std::mutex               sessions_mtx_;
-
-    std::thread      heartbeat_thread_;
-    std::atomic<bool> running_{true};
-    int              heartbeat_interval_ms_;
-};
+        std::thread heartbeat_thread_;
+        std::atomic<bool> running_{true};
+        int heartbeat_interval_ms_;
+    };
 
 } // namespace uds_hft
